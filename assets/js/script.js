4e4b0d3bf8fe8704373e07de9ee3f8e9148dce54
EldenBear <dontet1995@gmail.com>
2023-03-27 21:10:40 -0400

    various changes to html and js

--- assets/js/script.js
+++ assets/js/script.js
@@ -18,10 +18,23 @@ fetch(geo_url, {
         if (response.length > 0) {
             latitude = response[0].lat;
             longitude = response[0].lon;
-            is_city_valid = true;
-            console.log(latitude);
-            console.log(longitude);
-            var url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}`;
+            var current_url = `http://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${key}&units=imperial`;
+            fetch(current_url, {
+                method: 'GET', //GET is the default.
+                })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (response) {
+                  var date = dayjs().format('M/D/YYYY');
+                        $('#city-name').text(response.name + " " + date);
+                        $('#temp-icon').attr('src', "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+                        $('#city-temp').text("Temp: " + response.main.temp + "\u00B0F");
+                        $('#city-wind').text("Wind: " + response.wind.speed + "MPH");
+                        $('#city-humidity').text("Humidity: " + response.main.humidity + "%");
+                    });
+
+            var url = `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${key}&units=imperial`;
             fetch(url, {
                 method: 'GET', //GET is the default.
                 })
@@ -29,8 +42,20 @@ fetch(geo_url, {
                     return response.json();
                 })
                 .then(function (response) {
-                        console.log(response);
-                        
+                  console.log(response);
+                        for (let index = 1; index < 6; index++) {
+                          var calc_index = ((index-1) * 8)-1;
+                          if (calc_index < 0) {
+                            calc_index = 0;
+                          }
+                          var date = dayjs(response.list[calc_index].dt_txt.split(" ")[0]).format('M/D/YYYY');
+                          $(`#date-${index}`).text(date);
+                          $(`#temp-icon-${index}`).attr('src', "https://openweathermap.org/img/wn/" + response.weather[0].icon + "@2x.png");
+                          $(`#city-temp-${index}`).text("Temp: " + response.main.temp + "\u00B0F");
+                          $(`#city-wind-${index}`).text("Wind: " + response.wind.speed + "MPH");
+                          $(`#city-humidity-${index}`).text("Humidity: " + response.main.humidity + "%");
+                          
+                        }
                     })
         }
         
